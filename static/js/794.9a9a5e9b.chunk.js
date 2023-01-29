c1e7f331100c3b805d07bf0b3cd821637c5b7b62
IsaacQQ <74833446+IsaacQQ@users.noreply.github.com>
2023-01-29 12:50:01 +0000

    Deploying to gh-pages from @ IsaacQQ/goit-react-hw-08-phonebook@60175f1463172f6c11bee52d41e7dd4ca5814d0f 🚀

---
+++ static/js/794.9a9a5e9b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[794],{1794:function(e,o,n){n.r(o);var h=n(1087),t=n(3329);o.default=function(){return(0,t.jsxs)("div",{children:[(0,t.jsx)("h1",{children:"Page not found"}),(0,t.jsx)(h.rU,{to:"/*",children:"To home page"})]})}}}]);
+//# sourceMappingURL=794.9a9a5e9b.chunk.js.map
